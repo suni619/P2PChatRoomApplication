98956e6a2e00fcb4cf063456b13b5531292b6705
Jagadish S <jagadish099@yahoo.com>
2014-11-07 21:48:45 -0600

    Test 4

--- P2PChatRoomApplication/src/com/ds/project/server/ChatServer.java
+++ P2PChatRoomApplication/src/com/ds/project/server/ChatServer.java
@@ -4,6 +4,6 @@ public class ChatServer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi from Server");
-	
+		System.out.println("Success");	
 	}
 }
