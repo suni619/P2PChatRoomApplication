0147e70194937924e52180adfc9fefbe53db5e27
Jagadish Shivanna <jagadish030@gmail.com>
2014-11-07 21:11:56 -0600

    Test 2

---
+++ P2PChatRoomApplication/src/com/ds/project/server/ChatServer.java
@@ -0,0 +1,8 @@
+package com.ds.project.server;
+
+public class ChatServer {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi from Server");
+	}
+}
