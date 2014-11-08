3d8139446e7190fd1d3a32d6659841dbce290e0b
suni619 <suni619@gmail.com>
2014-11-07 21:31:31 -0600

    Conflicts resolved in Chat client

--- P2PChatRoomApplication/src/com/ds/project/client/ChatClient.java
+++ P2PChatRoomApplication/src/com/ds/project/client/ChatClient.java
@@ -4,11 +4,8 @@ public class ChatClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("Hi from Client");
-=======
 		System.out.println("Success.");
->>>>>>> branch 'master' of https://github.com/suni619/P2PChatRoomApplication.git
 	}
 
 }
